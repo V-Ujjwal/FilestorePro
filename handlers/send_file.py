76859e7286c37afdd1c533467aa1607a1b508abe
Ujjwal <125728251+V-Ujjwal@users.noreply.github.com>
2023-04-03 20:21:20 +0530

    Add files via upload

---
+++ handlers/send_file.py
@@ -0,0 +1,43 @@
+# (c) @AbirHasan2005 | @PredatorHackerzZ
+
+import asyncio
+from configs import Config
+from pyrogram import Client
+from pyrogram.types import Message
+from pyrogram.errors import FloodWait
+from handlers.helpers import str_to_b64
+
+
+async def reply_forward(message: Message, file_id: int):
+    try:
+        await message.reply_text(
+            f"**Here is Sharable Link of this file:**\n"
+            f"https://t.me/{Config.BOT_USERNAME}?start=MxaBots_{str_to_b64(str(file_id))}\n\n"
+            f"__To Retrive the Stored File, just open the link!__",
+            disable_web_page_preview=True, quote=True)
+    except FloodWait as e:
+        await asyncio.sleep(e.value)
+        await reply_forward(message, file_id)
+
+
+async def media_forward(bot: Client, user_id: int, file_id: int):
+    try:
+        if Config.FORWARD_AS_COPY is True:
+            return await bot.copy_message(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
+                                          message_id=file_id)
+        elif Config.FORWARD_AS_COPY is False:
+            return await bot.forward_messages(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
+                                              message_ids=file_id)
+    except FloodWait as e:
+        await asyncio.sleep(e.value)
+        return media_forward(bot, user_id, file_id)
+
+
+async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
+    sent_message = await media_forward(bot, user_id, file_id)
+    l = await reply_forward(message=sent_message, file_id=file_id)
+   w = await message.reply_text(f'‼️ File will auto delete in 2 minutes😱\n💡Forward it to saved massages or anywhere before downloading.😁\n😇Join @Movies_X_Animes')
+    await asyncio.sleep(120)
+    await sent_message.delete()
+    await l.delete()
+    await w.delete()
