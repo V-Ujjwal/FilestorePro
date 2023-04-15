5ea529c76c7c21c3624e317883981c942b37a55d
Ujjwal <125728251+V-Ujjwal@users.noreply.github.com>
2023-04-15 18:04:57 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -34,9 +34,8 @@ async def media_forward(bot: Client, user_id: int, file_id: int):
 
 
 async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
-    sent_message = await media_forward(bot, user_id, file_id)
-    #await reply_forward(message=sent_message, file_id=file_id)
+    sent_message = await media_forward(bot, user_id, file_id)   
     await asyncio.sleep(0.5)
     return sent_message
     await asyncio.sleep(20)
-    return sent_message.delete()
+    await sent_message.delete()
