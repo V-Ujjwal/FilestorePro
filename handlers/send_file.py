2b834366babe667eca5bd3e26afe5039f77a35f6
Ujjwalm2004 <130154620+Ujjwalm2004@users.noreply.github.com>
2023-04-23 01:38:49 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -6,7 +6,6 @@ from pyrogram import Client
 from pyrogram.types import Message
 from pyrogram.errors import FloodWait
 from handlers.helpers import str_to_b64
-from bot import bot
 import time
 
 async def reply_forward(message: Message, file_id: int):
@@ -38,7 +37,7 @@ async def delete_file(file_id: int):
     # Code to delete the file goes here
 
 
-async def send_media_and_replies(bot: Client, user_id: int, file_id: int):
+async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
     sent_message = await media_forward(bot, user_id, file_id)
     await reply_forward(message=sent_message, file_id=file_id)
     await sent_message.delete(delete_file(file_id))  # schedule the file deletion task
