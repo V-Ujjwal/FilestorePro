b81f4e9df82a93775ee5332da962d8dcf22b4542
Ujjwalm2004 <130154620+Ujjwalm2004@users.noreply.github.com>
2023-04-23 13:07:29 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -39,7 +39,16 @@ async def delete_file(file_id: int):
 
 async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
     sent_message = await media_forward(bot, user_id, file_id)
-    reply_forward(message=sent_message, file_id=file_id)
+    await reply_forward(message=sent_message, file_id=file_id)
     await asyncio.create_task(delete_file(file_id)) # schedule the file deletion task
-    
-    
+  
+      def send_files(bot, chat_id, file_ids):
+    for file_id in file_ids:
+        bot.send_chat_action(chat_id=chat_id, action=telegram.ChatAction.UPLOAD_DOCUMENT)
+        bot.send_document(chat_id=chat_id, document=file_id)
+        # Wait for 30 seconds before sending the next file
+        time.sleep(30)
+
+    # Delete all files after the last file has been sent
+    for file_id in file_ids:
+        bot.delete_message(chat_id=chat_id, message_id=file_id)
