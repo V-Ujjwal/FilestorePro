f356061d1aea80e9050d5f583f0d4cf49c98e8da
Ujjwalm2004 <130154620+Ujjwalm2004@users.noreply.github.com>
2024-04-14 16:35:45 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,6 +4,7 @@ import os
 
 
 class Config(object):
+	URL = os.getenv("URL", "")
 	API_ID = int(os.environ.get("API_ID", "0"))
 	API_HASH = os.environ.get("API_HASH")
 	BOT_TOKEN = os.environ.get("BOT_TOKEN")
